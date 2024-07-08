9067b67bc803ba984be55ed7c6807243e37042d4
unknown <a630312@wca.gov.tw>
2024-07-08 17:04:10 +0800

    final version

--- guess_number.py
+++ guess_number.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100)
+start = input('請輸入隨機範圍值的開始值：')
+end = input('請輸入隨機範圍值的結束值：')
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
 count = 0
 while True:
 	count +=1
